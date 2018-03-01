bf056752bf926d7d12a3cd4218a2d5ec858491f4
Kamil Wojtczak <kamil.wojtczak1@gmail.com>
2018-03-01 19:39:17 +0100

    Add node.js files

---
+++ modules/time.js
@@ -0,0 +1,11 @@
+var os = require('os');
+
+function getTime(uptime) {
+    var uptime = os.uptime().toFixed(0);
+    var h, min, sec;
+    h = Math.floor(uptime / (60*60));
+    min = Math.floor(uptime%(60*60) / 60);
+    sec = Math.floor(uptime % 60);
+    console.log('Uptime:', h, 'h', min, 'min', sec, 'sec');
+}
+exports.print = getTime;
